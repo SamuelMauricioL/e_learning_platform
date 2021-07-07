3daddba10c8b79be51649fde3248fdeaf008c302
nicoIYIZ <64280175+gerson-cudesi@users.noreply.github.com>
2021-07-07 16:29:27 -0500

    sidebar

--- src/app/UI/view-models/home/home.component.ts
+++ src/app/UI/view-models/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AuthService } from '../../../infraestructure/driven-adapter/auth/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +9,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public user$: Observable<any> = this.auth.afAuth.user;
+
+  constructor(private auth: AuthService) { }
 
   ngOnInit(): void {
     
