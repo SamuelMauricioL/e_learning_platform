c7dcacb4f505ffbd6a9b8ccb4deae34f05e16010
gersonmlb <gerson_159@outlook.es>
2021-11-24 16:56:38 -0500

    responder preguntas

--- src/app/UI/view-models/respuestas/respuestas.component.ts
+++ src/app/UI/view-models/respuestas/respuestas.component.ts
@@ -1,10 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { GetPreguntasUseCases } from 'src/app/domain/usecase/get-preguntas-use-case';
-import { GetRespuestasUseCases } from 'src/app/domain/usecase/get-respuestas-use-case';
-import { PreguntaModel } from 'src/app/domain/models/Pregunta/pregunta-model';
-import Stepper from 'bs-stepper';
+// import { PreguntaModel } from 'src/app/domain/models/Pregunta/pregunta-model';
 import { Observable } from 'rxjs';
 import { AuthService } from '../../../infraestructure/driven-adapter/auth/auth.service';
 
@@ -18,41 +15,35 @@ export class RespuestasComponent implements OnInit {
 
   public user$: Observable<any> = this.auth.afAuth.idToken;
   idPregunta: string = this.route.snapshot.params.idPregunta;
-  idtema: string = this.route.snapshot.params.idtema;
-  ruta: string = this.route.snapshot.params.ruta;
-
-  private stepper!: Stepper;
-
-
-  isLinear = false;
 
   constructor(
     private route: ActivatedRoute,
     private service_preguntas: GetPreguntasUseCases,
-    private service_respuestas: GetRespuestasUseCases,
     private auth: AuthService,
-  ) { this.managerForm = new FormGroup({}); }
+  ) { }
 
-  managerForm: FormGroup;
-
-  // collection_de_preguntas = [] as PreguntaModel[];
   collection_de_preguntas_completo = Array();
-  collection_de_respuestas = Array();
-  collection_de_respuestas_leng = 0;
-  rpta = 0;
-  crct = 0;
-  eval = 0;
+
+  rpta_correcta = 0;
+  rpta_incorrecta = 0;
 
   sec = 0;
   min = 0;
   hrs = 0;
   tiempo_transcurrido = '';
-  
 
-  notas = Array();
+  timePassed: any;
+  timeLeft: number = 0;
+
+  interval: any;
+  alerts: number = 0;
+
+  rpta_modal: number = 0;
+  rpta_si_no: number = 0;
+
 
   ngOnInit(): void {
-        
+
     this.service_preguntas.getAll(this.idPregunta).subscribe(pregunta => {
       for (let i = 0; i < pregunta.length; i++) {
         this.service_preguntas.getAllAlternative(pregunta[i].id).subscribe((alternativa: any) => {
@@ -72,21 +63,12 @@ export class RespuestasComponent implements OnInit {
           });
         });
       }
-      
       this.timer();
-      
     },
       error => {
         console.error(error);
       });
   }
-
-  timePassed: any;
-  timeLeft: number = 0;
-
-  interval: any;
-
-  
 
   pauseTimer() {
     clearInterval(this.interval);
@@ -100,74 +82,44 @@ export class RespuestasComponent implements OnInit {
     return H + ":" + M + ":" + S;
   }
 
-  evaluar() {
-    if (this.rpta == this.crct) {
-      this.notas.push(20);
-      this.eval = 1;
-      console.log("Correcto");
-
-    } else if (this.rpta != this.crct) {
-      this.notas.push(0);
-      this.eval = 2;
-      console.log("inCorrecto");
-    }
-  }
-
-  tick(){
+  tick() {
     this.sec++;
     if (this.sec >= 60) {
-        this.sec = 0;
-        this.min++;
-        if (this.min >= 60) {
-            this.min = 0;
-            this.hrs++;
-        }
+      this.sec = 0;
+      this.min++;
+      if (this.min >= 60) {
+        this.min = 0;
+        this.hrs++;
+      }
     }
   }
   add() {
     this.tick();
-    this.tiempo_transcurrido = (this.hrs > 9 ? this.hrs : "0" + this.hrs) 
-        	 + ":" + (this.min > 9 ? this.min : "0" + this.min)
-       		 + ":" + (this.sec > 9 ? this.sec : "0" + this.sec);        
+    this.tiempo_transcurrido = (this.hrs > 9 ? this.hrs : "0" + this.hrs)
+      + ":" + (this.min > 9 ? this.min : "0" + this.min)
+      + ":" + (this.sec > 9 ? this.sec : "0" + this.sec);
     this.timer();
   }
-  timer() {    
-    setTimeout(()=>{ this.add(); },1000);
-
-  }
-  finalizar() {
+  timer() {
+    setTimeout(() => { this.add(); }, 1000);
 
   }
 
-  guardar(): void {
-    let total = this.notas.reduce((a, b) => a + b, 0);
-    let sumatotal = total / this.notas.length;
+  evaluar(resp: any) {
+    if (resp[1] == "si") {
+      this.rpta_si_no = 1;
+      this.rpta_modal=1;
+      this.rpta_correcta++;
 
-    this.managerForm = new FormGroup({
-      id: new FormControl('FEZHR6U3jWrlqUrH33mF', Validators.required),
-      identificador: new FormControl('FEZHR6U3jWrlqUrH33mF', Validators.required),
-      idAlumno: new FormControl('prueba', Validators.required),
-      idTema: new FormControl(this.idtema, Validators.required),
-      promedio: new FormControl(sumatotal, Validators.required),
-      ruta: new FormControl(this.ruta, Validators.required),
-      tiempo: new FormControl('00:00:40', Validators.required),
-      estado: new FormControl(true, Validators.required),
-    });
-
-    const { id, ...obj } = this.managerForm.value;
-    this.service_respuestas.create(obj).then((_response) => {
-      this.managerForm.reset();
-    }).catch((error) => {
-      console.error(error);
-    });
+    } else if (resp[1] == "no") {
+      this.rpta_si_no = 0;
+      this.rpta_modal=1;
+      this.rpta_incorrecta++;
+    }
   }
 
-  next() {
-    this.stepper.next();
+  cerrarAlert(close: any) {
+    this.rpta_modal = 0;
   }
 
-  onSubmit() {
-    // do something here
-    return false;
-  }
 }
--- src/app/UI/view-models/respuestas/steps-preguntas/steps-preguntas.component.ts
+++ src/app/UI/view-models/respuestas/steps-preguntas/steps-preguntas.component.ts
@@ -1,4 +1,8 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter, ElementRef } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { GetRespuestasUseCases } from 'src/app/domain/usecase/get-respuestas-use-case';
+import { MatStepper } from '@angular/material/stepper';
 
 @Component({
   selector: 'app-steps-preguntas',
@@ -8,23 +12,66 @@ import { Component, Input, OnInit } from '@angular/core';
 export class StepsPreguntasComponent implements OnInit {
 
   @Input() preguntas!: Array<any>;
+  @Output() correct = new EventEmitter<[number, string]>();
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private service_respuestas: GetRespuestasUseCases,
+    private el: ElementRef
+  ) { this.managerForm = new FormGroup({}); }
+
+  isLinear = false;
+
+  ruta: string = this.route.snapshot.params.ruta;
+  idtema: string = this.route.snapshot.params.idtema;
+  managerForm: FormGroup;
+  notas = Array();
+
 
   ngOnInit() {
-    console.log("this.preguntas")
-    console.log(this.preguntas)
-    // //@ts-ignore
-    // this.steppers = new Stepper((document.querySelector('#stepper1')), {
-    //   linear: false,
-    //   animation: true
-    // })
   }
 
-  onSelect(respuesta: string, correcto: string) {
-    console.log(respuesta);
-    console.log(correcto);
-    // this.rpta = parseInt(respuesta);
-    // this.crct = parseInt(correcto);
+  onSelect(idelement: string, respuesta: string) {
+    let myTag = this.el.nativeElement.querySelector(`.${idelement}`);
+    if(respuesta == "si"){
+      this.correct.emit([1, respuesta]);
+      myTag.classList.add('correct');
+    }else{
+      myTag.classList.add('incorrect');
+      this.correct.emit([1, respuesta]);
+    }
   }
+
+  guardar(): void {
+    let total = this.notas.reduce((a, b) => a + b, 0);
+    let sumatotal = total / this.notas.length;
+
+    this.managerForm = new FormGroup({
+      id: new FormControl('FEZHR6U3jWrlqUrH33mF', Validators.required),
+      identificador: new FormControl('FEZHR6U3jWrlqUrH33mF', Validators.required),
+      idAlumno: new FormControl('prueba', Validators.required),
+      idTema: new FormControl(this.idtema, Validators.required),
+      promedio: new FormControl(sumatotal, Validators.required),
+      ruta: new FormControl(this.ruta, Validators.required),
+      tiempo: new FormControl('00:00:40', Validators.required),
+      estado: new FormControl(true, Validators.required),
+    });
+
+    const { id, ...obj } = this.managerForm.value;
+    this.service_respuestas.create(obj).then((_response) => {
+      this.managerForm.reset();
+    }).catch((error) => {
+      console.error(error);
+    });
+  }
+
+  public nextfunction (stepper: MatStepper) {
+    // stepper:MatStepper
+    // stepper.next();
+    console.log("stepper")
+    console.log(stepper)
+
+  }
+
+
 }
